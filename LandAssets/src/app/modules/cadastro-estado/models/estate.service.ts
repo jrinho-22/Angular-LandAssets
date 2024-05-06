c548207d1c657479638bf9f28444d570b5b04f79
Junior <jrinho22@gmail.com>
2024-05-06 10:43:25 -0300

    updates

---
+++ LandAssets/src/app/modules/cadastro-estado/models/estate.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpRequestService } from 'src/app/services/HttpRequest.service';
+import { HttpClient } from '@angular/common/http';
+import resources from '../../../config';
+
+@Injectable()
+export class EstateModel extends HttpRequestService {
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  config() {
+    return {
+      resource: resources.ESTATE,
+    };
+  }
+}

7b7d3af8aa843013eed29a7a9f769ea048e36456
unknown <jrinho22@gmail.com>
2024-07-24 20:58:05 -0300

    feat: credential info

--- LandAssets/src/app/app-routing.module.ts
+++ LandAssets/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import dashboardRoute from './views/dashboard/routes';
 import cadastroEstadoRoute from './modules/estado/routes';
-import loginRoute from './views/login/routes';
 import { plotRoutes } from './modules/plot/routes';
 import clientPlots  from './modules/client-plot/routes';
 import { salesRoutes } from './modules/sales/routes';
